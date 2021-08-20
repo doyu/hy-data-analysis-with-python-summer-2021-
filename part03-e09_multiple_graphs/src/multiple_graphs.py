9b524eda423b7092afe95f528dba54b95e552b81
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2021-08-20 16:20:23 +0300

    update

--- part03-e09_multiple_graphs/src/multiple_graphs.py
+++ part03-e09_multiple_graphs/src/multiple_graphs.py
@@ -3,7 +3,19 @@
 import matplotlib.pyplot as plt
 
 def main():
-    pass
+
+    plt.title("My first figure")  # Add a title to the figure
+    plt.xlabel("My x-axis")       # Give a label to the x-axis
+    plt.ylabel("My y-axis")       # Give a label to the y-axis
+
+    x1 = [2,4,6,7]
+    y1 = [4,3,5,1]
+    x2 = [1,2,3,4]
+    y2 = [4,2,3,1]
+
+    plt.plot(x1, y1)
+    plt.plot(x2, y2)
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- part03-e10_subfigures/src/subfigures.py
+++ part03-e10_subfigures/src/subfigures.py
@@ -4,10 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def subfigures(a):
-    pass
+    plt.subplot(1, 2, 1)    # Note the 1-indexing of subplots.
+    plt.plot(a[:,0], a[:,1], c=a[:,2])
+    plt.subplot(1, 2, 2)
+    plt.scatter(a[:,0], a[:,1], c=a[:,2], s=a[:,3])
+    plt.show()
 
 def main():
-    pass
+    subfigures(np.random.random((30,4)))
 
 if __name__ == "__main__":
     main()
