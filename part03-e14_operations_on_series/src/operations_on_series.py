27957ee9468e455efca34f67cc6773866675528f
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2021-08-24 09:22:30 +0300

    finished week 3 && registered

--- part03-e14_operations_on_series/src/operations_on_series.py
+++ part03-e14_operations_on_series/src/operations_on_series.py
@@ -1,14 +1,48 @@
 #!/usr/bin/env python3
 
+'''
+Exercise 14 (operations on series)
+Write function "create_series(L1, L2)" that gets two lists of numbers as
+parameters. Both lists should have length 3. The function should first
+create two Series, s1 and s2. The first series should have values from
+the first parameter list and have corresponding indices a, b, and c.
+The second series should get its values from the second parameter list
+and have again the corresponding indices a, b, and c. The function
+should return the pair of these Series.
+
+Then, write a function "modify_series(s1, s2)" that gets two Series as
+parameters. It should add to the first Series s1 a new value with
+index d. The new value should be the same as the value in Series s2
+with index b. Then delete the element from s2 that has index b. Now
+the first Series should have four values, while the second list has
+only two values. Adding a new element to a Series can be achieved by
+assignment, like with dictionaries. Deletion of an element from a
+Series can be done with the del statement.
+
+Test these functions from the main function. Try adding together the
+Series returned by the modify_series function. The operations on
+Series use the indices to keep the element-wise operations aligned. If
+for some index the operation could not be performed, the resulting
+value will be NaN (Not A Number).
+
+'''
+import pandas as pd
 
 def create_series(L1, L2):
-    return (None, None)
-    
+    return (pd.Series(L1, index=list("abc")), pd.Series(L2, index=list("abc")))
+
 def modify_series(s1, s2):
-    return (None, None)
-    
+    s1["d"] = s2["b"]
+    s2 = s2.drop("b")
+    return (s1, s2)
+
 def main():
+    s1, s2 = create_series([11,12,13], [21,22,23])
+    ss1, ss2 = modify_series(s1, s2)
+    print(ss1)
+    print(ss2)
+    print(ss1 + ss2)
     return
-    
+
 if __name__ == "__main__":
     main()
--- part03-e15_inverse_series/src/inverse_series.py
+++ part03-e15_inverse_series/src/inverse_series.py
@@ -1,11 +1,48 @@
 #!/usr/bin/env python3
 
+'''
+Exercise 15 (inverse series)
+Write function inverse_series that get a Series as a parameter and
+returns a new series, whose indices and values have swapped roles.
+Test your function from the main function.
+
+What happens if some value appears multiple times in the original
+Series? What happens if you use this value to index the resulting
+Series?
+
+One may notice that there are similarities between Python’s
+dictionaries and Pandas’ Series, both can be thought to access values
+using keys. The difference is that Series requires that the indices
+have all the same type, and similarly, all the values have the same
+type. This restriction allows creation of fast data structures.
+
+As a mark of the similaries between these two data structures, Pandas
+allows creation of a Series object from a dictionary:
+
+
+[24]:
+d = { 2001 : "Bush", 2005: "Bush", 2009: "Obama", 2013: "Obama", 2017 : "Trump"}
+s4 = pd.Series(d, name="Presidents")
+s4
+[24]:
+2001     Bush
+2005     Bush
+2009    Obama
+2013    Obama
+2017    Trump
+Name: Presidents, dtype: object
+
+'''
+
 import pandas as pd
 
 def inverse_series(s):
-    return pd.Series()
+#    print(s.index)
+#    print(s.values)
+    return pd.Series(s.index, index=s.values)
 
 def main():
+    print(inverse_series(pd.Series([1,2,3],index=list("abc"))))
     return
 
 if __name__ == "__main__":
