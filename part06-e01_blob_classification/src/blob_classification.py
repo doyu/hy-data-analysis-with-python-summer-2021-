c5703c09cc8717d7f1ceec96fb4e972870103ac9
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2021-08-30 20:19:43 +0300

    update

--- part06-e01_blob_classification/src/blob_classification.py
+++ part06-e01_blob_classification/src/blob_classification.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+'''Exercise 1 (blob classification)
+
+Write function "blob_classification()" that gets feature matrix X and
+label vector y as parameters. It should then return the accuracy score
+of the prediction. Do the prediction using GaussianNB, and use
+"train_test_split()" function from sklearn to split the dataset in to two
+parts: one for training and one for testing. Give parameter
+random_state=0 to the splitting function so that the result is
+deterministic. Use training set size of 75% of the whole data.
+'''
+
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
@@ -7,7 +18,10 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
 def blob_classification(X, y):
-    return 0.0
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+    model = GaussianNB()
+    model.fit(X_train, y_train)
+    return metrics.accuracy_score(y_test, model.predict(X_test))
 
 def main():
     X,y = datasets.make_blobs(100, 2, centers=2, random_state=2, cluster_std=2.5)
--- part06-e02_plant_classification/src/plant_classification.py
+++ part06-e02_plant_classification/src/plant_classification.py
@@ -1,12 +1,45 @@
 #!/usr/bin/env python3
 
+'''Exercise 2 (plant classification)
+Write function "plant_classification()" that does the following:
+
+* loads the iris dataset using sklearn (sklearn.datasets.load_iris)
+* splits the data into training and testing part using the
+  train_test_split function so that the training set size is 80% of
+  the whole data (give the call also the random_state=0 argument to
+  make the result deterministic)
+* use Gaussian naive Bayes to fit the training data
+* predict labels of the test data
+* the function should return the accuracy score of the prediction
+  performance (sklearn.metrics.accuracy_score)
+'''
+
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn import naive_bayes
 from sklearn import metrics
 
 def plant_classification():
-    return 0.0
+    # * loads the iris dataset using sklearn (sklearn.datasets.load_iris)
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+    # * splits the data into training and testing part using the
+    #   "train_test_split()" function so that the training set size is 80% of
+    #   the whole data (give the call also the random_state=0 argument to
+    #   make the result deterministic)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # * use Gaussian naive Bayes to fit the training data
+    model = naive_bayes.GaussianNB()
+    model.fit(X_train, y_train)
+
+    # * predict labels of the test data
+    y_predicted = model.predict(X_test)
+
+    # * the function should return the accuracy score of the prediction
+    #   performance (sklearn.metrics.accuracy_score)
+    return metrics.accuracy_score(y_test, y_predicted)
 
 def main():
     print(f"Accuracy is {plant_classification()}")
