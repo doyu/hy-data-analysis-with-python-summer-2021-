e44f1cfa813b9315ce14abccfd784772dcbfa758
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2021-09-08 16:03:23 +0300

    all done, I suppose

--- part03-e12_radial_fade/src/radial_fade.py
+++ part03-e12_radial_fade/src/radial_fade.py
@@ -2,55 +2,41 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 def center(a):
-    ny,nx = a.shape[:2]
-    return  ((ny-1)/2, (nx-1)/2)   # note the order: (center_y, center_x)
+    return  ((a.shape[0]-1)/2, (a.shape[1]-1)/2)
 
 def radial_distance(a):
-    # https://tmc.mooc.fi/paste/fEQWDPKUpxJFUt5BBMc42w
-    rows = np.arange(a.shape[0])
-    cols = np.arange(a.shape[1])
-    y, x = np.meshgrid(rows,cols,indexing='ij')
     yc, xc = center(a)
-    dist = (((y-yc)**2) + ((x-xc)**2))**0.5
-    return dist
+    ret = np.array([math.sqrt((y-yc)**2 + (x-xc)**2)
+                    for x in range(a.shape[1])
+                    for y in range(a.shape[0])]).reshape(a.shape[:2])
+    return ret
 
 def scale(a, tmin=0.0, tmax=1.0):
     """Returns a copy of array 'a' with its values scaled to be in the range
     [tmin,tmax]."""
-    if (a.max() == a.min()).all:
+    old_range = a.max() - a.min()
+    if old_range == 0:
         return a
-    old_range = a.max() == a.min()
     new_range = tmax - tmin
     return (a - a.min()) *  new_range / old_range + tmin
 
 def radial_mask(a):
-    dist = radial_distance(a)
-    dist_scaled = scale(dist)
-    return 1 - dist_scaled
+    ret = 1 - scale(radial_distance(a))
+    return np.where(ret < 1.0, 1.0, ret)
 
 def radial_fade(a):
     return a * radial_mask(a)[:,:,np.newaxis]
 
 def main():
-    n = 1
-    m = 2
-    a = np.random.randn(n,m,3)
-    result = radial_fade(a)
-    cy = (n-1) // 2
-    cx = (m-1) // 2
-    print(result[cy,cx])
-    print(a[cy,cx])
-    assert (result[cy,cx] == a[cy,cx]).all(), "In the center of the image there should be no fading"
-
-#    painting = plt.imread("src/painting.png")
-#    plt.subplot(3, 1, 1)
-#    plt.imshow(painting)
-#    plt.subplot(3, 1, 2)
-#    plt.imshow(radial_mask(painting))
-#    plt.subplot(3, 1, 3)
-#    plt.imshow(radial_fade(painting))
+    painting = plt.imread("src/painting.png")
+    img = painting.copy()
+    fig, ax = plt.subplots(3,1)
+    ax[0].imshow(painting)
+    ax[1].imshow(radial_mask(img))
+    ax[2].imshow(radial_fade(img))
     plt.show()
 
 if __name__ == "__main__":
